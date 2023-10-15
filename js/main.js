dbd28c09ad3c586fd393ee97951d5426feacd017
josemariaovejeroastorga <ing.josemariaovejeroastorga@gmail.com>
2023-10-15 12:14:33 -0300

    Actualizacion 15/10/2023

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-<script>
-    var nombre = prompt("Ingrese su nombre", "")
-    document.write("Hola " + nombre)
-</script>
+
+    /*var nombre = prompt("Por favor escriba su nombre", "");
+    var apellido = prompt("Por favor escriba su apellido","");
+    var concatenacion = nombre + " " + apellido;
+            document.write ("Hola " + concatenacion)*/
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
